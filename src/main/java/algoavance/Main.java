377771a0a50a4b2c71b785469d378784041287a2
FloQuantic <flo.thibaut@gmail.com>
2019-11-30 10:26:53 +0100

    UBQP sans tabou fini

--- src/main/java/algoavance/Main.java
+++ src/main/java/algoavance/Main.java
@@ -1,15 +1,54 @@
 package algoavance;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main
 {
+
+    private static int constraint;
+
+    public static int[][] chargerQ(String file){
+        int[][] monQ = new int[0][];
+        try{
+            InputStream flux=new FileInputStream(file);
+            InputStreamReader lecture=new InputStreamReader(flux);
+            BufferedReader buff=new BufferedReader(lecture);
+            String ligne;
+            while ((ligne=buff.readLine())!=null){
+                monQ = ligneTokenizer(ligne);
+            }
+            buff.close();
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+        }
+        return monQ;
+    }
+
+    private static int[][] ligneTokenizer(String ligne) {
+        int[][] monQ;
+        StringTokenizer st =  new StringTokenizer(ligne);
+        int len = Integer.parseInt(st.nextToken(" "));
+        monQ = new int[len][len];
+        constraint = Integer.parseInt(st.nextToken(" "));
+        for (int i=0; i<len; i++){
+            for (int j=0; j<len; j++){
+                monQ[i][j]=Integer.parseInt(st.nextToken(" "));
+            }
+        }
+        return monQ;
+    }
+
     public static void main( String[] args )
     {
-        int[][] Q = new int[6][6];
+        int[][] Q = chargerQ("graphe12345.txt");
 
         UBQP myUBQP = new UBQP(Q);
+        System.out.println(myUBQP.QtoString());
+        System.out.println(myUBQP.tabToString(myUBQP.hillClimbingWithRestart()) + "\nf=" + myUBQP.getF());
     }
 }
--- src/main/java/algoavance/UBQP.java
+++ src/main/java/algoavance/UBQP.java
@@ -1,33 +1,149 @@
 package algoavance;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class UBQP {
 
+    private final static int NB_DEPL = 100;
+    private final static int NB_ESSAIS = 100;
     private int[] X;
     private int[][] Q;
+    private int f;
+    private int qLenght;
 
     public UBQP(int[][] Q){
         this.Q = Q;
-        X = new int[Q[0].length];
+        qLenght= Q[0].length;
+        X = new int[qLenght];
         solutionAleatoire();
+    }
+
+    public String XtoString(){
+        String toReturn = "X:\n";
+        toReturn += tabToString(X);
+        return toReturn;
+    }
+
+    public String QtoString(){
+        String toReturn = "Q:\n";
+        toReturn += matToString(Q);
+        return toReturn;
+    }
+
+    public String tabToString(int[] tab){
+        String toReturn = "[\t";
+        for (int i=0; i<tab.length; i++){
+            toReturn+= tab[i] + "\t";
+        }
+        toReturn += "]";
+        return toReturn;
+    }
+
+    public String matToString(int[][] mat){
+        String toReturn = "";
+        for (int i=0; i<mat[0].length; i++){
+            toReturn += "[\t";
+            for (int j=0; j<mat[0].length; j++){
+                toReturn += mat[i][j] + "\t";
+            }
+            toReturn += "]\n";
+        }
+        return toReturn;
+    }
+
+    public int getF(){
+        f = calculerf(X);
+        return f;
+    }
+
+    private int calculerf(int[] vec){
+        int f=0;
+        for (int i=0; i<vec.length; i++){
+            for (int j=0; j<vec.length; j++){
+                f+=Q[i][j]*vec[i]*vec[j];
+            }
+        }
+        return f;
     }
 
     public void solutionAleatoire(){
         Random r = new Random();
         for (int i=0; i<X.length; i++){
-            X[i] = r.nextInt(100);
+            X[i] = r.nextInt(2);
         }
+        f=calculerf(X);
     }
 
-    public int calculerf(){
-        int f=0;
+    private int[][] voisinsX(){
+        int[][] voisinsX = new int[X.length][X.length];
         for (int i=0; i<X.length; i++){
             for (int j=0; j<X.length; j++){
-                f+=Q[i][j]*X[i]*X[j];
+                if (i==j) {
+                    voisinsX[i][j] = (X[j] + 1) % 2;
+                } else {
+                    voisinsX[i][j] = X[j];
+                }
             }
         }
-        return f;
+        return voisinsX;
+    }
+
+    private int[] meilleurVoisin(){
+        int[][] voisins = voisinsX();
+        int[] fVoisins=  new int[X.length];
+        for (int i=0; i<X.length; i++){
+            fVoisins[i]=calculerf(voisins[i]);
+        }
+
+        ArrayList<Integer> indexList = new ArrayList<>();
+        int min = fVoisins[0];
+        indexList.add(0);
+        for (int i=1; i<X.length; i++){
+            if (fVoisins[i]<min){
+                min = fVoisins[i];
+                indexList.clear();
+                indexList.add(i);
+            } else if (fVoisins[i]==min){
+                indexList.add(i);
+            }
+        }
+
+        Random r = new Random();
+        return voisins[indexList.get(r.nextInt(indexList.size()))];
+    }
+
+    private int[] steepestHillClimbing(){
+        int nbDepl = 0;
+        boolean stop = false;
+        int[] temp = X;
+
+        do {
+            temp = meilleurVoisin();
+            if (calculerf(temp)<calculerf(X)){
+                X = temp;
+            } else {
+                stop = true;
+            }
+            nbDepl++;
+        } while (nbDepl!=NB_DEPL && !stop);
+
+        return X;
+    }
+
+    public int[] hillClimbingWithRestart(){
+        int[] min = X;
+
+        for (int i=0; i<NB_ESSAIS; i++){
+            solutionAleatoire();
+            if (calculerf(steepestHillClimbing())<calculerf(min)){
+                min = X;
+            }
+        }
+
+        X = min;
+
+        return X;
     }
 
 
